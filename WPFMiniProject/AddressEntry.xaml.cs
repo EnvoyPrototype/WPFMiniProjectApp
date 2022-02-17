e53c35d556c7b0a45ce32e85094c321c56f1f7a7
James Rogers <jimrogers3315@gmail.com>
2022-02-17 06:05:38 -0600

    create AddressEntry form + visual layout

--- WPFMiniProject/AddressEntry.xaml.cs
+++ WPFMiniProject/AddressEntry.xaml.cs
@@ -1,10 +1,6 @@
-﻿using DemoLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,23 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPFMiniProject
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddressEntry.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddressEntry : Window
     {
-        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
-
-        public MainWindow()
+        public AddressEntry()
         {
             InitializeComponent();
-
-            addressesList.ItemsSource = addresses;    
         }
     }
 }
